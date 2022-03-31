2ca632c266ffd5d7a9b69024812b39635c3892db
Jonathan Nguyen <njonathan990@gmail.com>
2022-03-31 15:31:09 -0500

    cart.js base done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -18,13 +18,33 @@ function addItem( item, array){
         }
     }
 }   //end function
-// test to call the function //
+// test to call the function addItem//
 let fruit = 'apple';
-console.log ('checking if apple in basket:', addItem(fruit,basket)); //end test
+console.log ('checking if apple in basket:', addItem(fruit,basket)); 
+let veggie = 'carrot';
+console.log ('checking if apple and carrot are in basket', addItem(veggie, basket)); //end test
 // 
 // - Create a function called `listItems`. It should:
 //   - loop over the items in the `basket` array
 //   - console.log each individual item on a new line
-function listItems(){
-
+function listItems(array){
+    for (let i=0; i < array.length; i++){
+        console.log(array[i]);        
+    }
+    return "list of items complete";
 }   //end function
+// test to call the function listItem //
+console.log('items in the list: ');
+console.log(listItems(basket)); //end test
+// 
+// - Create a function called `empty`. It should:
+//   - reset the `basket` to an empty array
+function empty(array){
+    while(array.length){
+        array.shift();
+    }
+    return 'to empty list';
+} //end reset array
+// test to call the function empty //
+console.log(empty(basket));
+console.log(basket);
